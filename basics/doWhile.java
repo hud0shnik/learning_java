d850b94e06be97db5428e5f73ec863a1e73605db
Danila Egorov <c4nsero@mail.ru>
2022-04-28 19:51:07 +0300

    add doWhile

---
+++ basics/doWhile.java
@@ -0,0 +1,15 @@
+package java_app;
+
+public class App {
+
+    public static void main(String[] args) {
+        int j = 8;
+        do {
+            System.out.printf("i:\t%d\n",j);
+            j--;
+        }
+        while (j >= 0);
+        
+        System.out.println("Done.");
+    }
+}
