7c32c27041f1282b6d1098a74b1dc52f4bd25d44
Danila Egorov <c4nsero@mail.ru>
2022-04-28 19:42:07 +0300

    add for

---
+++ for/app/src/main/java/java_app/App.java
@@ -0,0 +1,12 @@
+package java_app;
+
+public class App {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 8; i++){
+            System.out.printf("Итерация № %d \n", i);
+        }
+        
+        System.out.println("Done.");
+    }
+}
