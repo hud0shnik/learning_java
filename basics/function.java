f70ad83b3cdac9ea7222b9321de6b66771ee89a1
Danila Egorov <c4nsero@mail.ru>
2022-04-29 16:55:03 +0300

    add function

---
+++ basics/function.java
@@ -0,0 +1,15 @@
+package java_app;
+
+public class App {
+    
+    
+    public String get8(){
+        return "8";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().get8());
+    }
+    
+    
+}
