f89b4b8d5f478666a8d9feaa8a59939468f2c4f6
Danila Egorov <c4nsero@mail.ru>
2022-04-29 16:14:13 +0300

    update if: minor fixes
    Подредачил вывод

--- basics/if.java
+++ basics/if.java
@@ -6,13 +6,13 @@ public class App {
         
         for (int i = 0; i <= 8; i++){
             if ( i==1) { 
-                    System.out.println("Catch 1");
+                    System.out.println("Got 1");
             }
             else if (i==5) {
-                    System.out.println("Catch 5");
+                    System.out.println("Got 5");
             }
             else if (i==8) {
-                    System.out.println("Catch 8");
+                    System.out.println("Got 8");
             }
             else {
                     System.out.printf("Number: %d\n", i);
