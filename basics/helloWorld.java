4423a75eb82f0d403580d400a607bf21e6c31a95
Danila Egorov <c4nsero@mail.ru>
2022-04-29 16:55:22 +0300

    update helloWorld
    Упростил

--- basics/helloWorld.java
+++ basics/helloWorld.java
@@ -1,11 +1,7 @@
 package helloWorld;
 
 public class App {
-    public String getGreeting() {
-        return "101";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("101!");
     }
 }
