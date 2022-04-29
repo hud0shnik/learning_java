d887ba76ecef058179409cee1b7d3c475b653011
Danila Egorov <c4nsero@mail.ru>
2022-04-29 16:46:11 +0300

    add switch

---
+++ basics/switch.java
@@ -0,0 +1,32 @@
+package java_app;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+       
+        for (int i = 0; i <= 8; i++){
+            switch(i){
+                case 0:            
+                    System.out.println("Got 0!");
+                    break;
+                case 2:            
+                    System.out.println("Got 2!");
+                    break;
+                case 4:            
+                    System.out.println("Got 4!");
+                    break;
+                case 8:            
+                    System.out.println("Got 8!");
+                    break;
+                default:            
+                    System.out.printf("Number is %d\n", i);
+            }
+        }
+        
+        
+        
+    }
+    
+}
