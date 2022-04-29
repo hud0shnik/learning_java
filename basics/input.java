28b68c1082678da7ddf3ad480a811b65b0f7620e
Danila Egorov <c4nsero@mail.ru>
2022-04-29 16:05:12 +0300

    update input.java: minor fixes
    Исправил опечатки и форматнул код

--- basics/input.java
+++ basics/input.java
@@ -6,10 +6,10 @@ public class App {
 
     public static void main(String[] args) {
         
-       Scanner scaner0 = new Scanner(System.in);
+        Scanner scanner0 = new Scanner(System.in);
         System.out.printf("Enter string:\t");
 
-        String input_str = scaner0.nextLine();
+        String input_str = scanner0.nextLine();
         System.out.println("Input is: " + input_str);
         
     }
